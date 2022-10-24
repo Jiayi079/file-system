6ab1bfb7cd617dcb841ce87e59b3d4fc740af21a
akizhao614 <jzhao19@sfsu.edu>
2022-10-23 21:55:53 -0700

    update team info

--- b_io.c
+++ b_io.c
@@ -1,9 +1,9 @@
 /**************************************************************
 * Class:  CSC-415-0# Fall 2021
-* Names: 
-* Student IDs:
-* GitHub Name:
-* Group Name:
+* Names:Jiaming Zhao, Jiayi Gu, Carmelo De Guzman, Congcheng Zeng
+* Student IDs:921891383, 920024739, 918749005, 918327792
+* GitHub Name:akizhao614, Jiayi079, carmelodz, Congchengz
+* Group Name:JCJC
 * Project: Basic File System
 *
 * File: b_io.c
