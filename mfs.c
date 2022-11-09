6eb50e2095351d0389848650711055c2df724772
Student <cdeguzman@mail.sfsu.edu>
2022-11-08 20:15:59 -0800

    Added in code comments

--- mfs.c
+++ mfs.c
@@ -41,17 +41,23 @@ char * fs_getcwd(char * pathname, size_t size){
 //Function to set current working directory(Cwd)
 int fs_setcwd(char * pathname){
 
+    //Create an array to hold our new CWD path
     char go_ToThisDir[MAX_PATHSIZE];
 
+    //Check if the passed-in directory is the root directory
+    //if it is, then copy it into our new CWD path array
     if(pathname[0] == Delim){
 
         strcpy(go_ToThisDir, pathname);
 
     } else {
 
+        //Check if the pathname starts with "..", if so
+        //then copy this as the parent directory in our new CWD path
         if (pathname[0] == "." && pathname[1] == "."){
 
             char * parent_Dir_ptr = get_parent_path(cwd);
+
             if(parent_Dir_ptr == NULL){
 
                 printf("Failed to get parent directory's pathname\n");
@@ -60,20 +66,31 @@ int fs_setcwd(char * pathname){
 
         strcpy(go_ToThisDir, parent_Dir_ptr);
 
+        /**************Start of Parsing Directory Path******************/
+
+        //If the pathname starts with ".", then it is the root
+        //directory. If so, then copy this as root in our new CWD
         } else if (pathname[0] == "."){
 
             strcpy(go_ToThisDir, cwd);
 
         } else {
 
+            //Otherwise, if the pathname is not root, then just
+            //copy the rest of the directory's path into our new CWD path
             strcpy(go_ToThisDir, cwd);
 
+        //Checks if our CWD path array only has the root directory, 
+        //if it does not, then parse each directory by concatting with 
+        //our delim "/"
         if(strlen(go_ToThisDir) != 1){
 
             strcat(go_ToThisDir, Delim);
 
         } if(pathname[0] == "." && pathname[1] == "."){
 
+                //Check to see if the CWD exists by calling getcwd() function,
+                //if not, then print error message 
                 if(getcwd(pathname, cwd) == NULL){
 
                     printf("Failed to get the CWD\n");
@@ -83,20 +100,30 @@ int fs_setcwd(char * pathname){
 
         }
 
+            //Parse together the path string into our new CWD path array
             strcat(go_ToThisDir, pathname);
 
         }
 
     }
 
+    /*****************End of Parsing Directory Path********************/
+
+    //Init a temporary placeholder for a dir_entry
    Directory_Entry * placeholder_DirEntry;
 
+    //Init a directory index that will search through our new CWD array
    int direc_Index = getDirIndex(placeholder_DirEntry, go_ToThisDir);
+
+   //If the directory is empty, meaning the index is -1, 
+   //then print an error message 
    if(direc_Index == -1){
 
     printf("Failed: this directory is not found\n");
     return -1;
 
+    //Check if directory entry is of type Directory,
+    //if not then print error
    } else if (directories[direc_Index].fileType != 0){
 
     printf("Current entry/item is not of type Directory\n");
@@ -104,7 +131,11 @@ int fs_setcwd(char * pathname){
 
    }
 
+    //Finally, copy this new CWD into the file system's cwd
     strcpy(cwd, go_ToThisDir);
-    return 0;
 
+    //Print the new CWD
+    printf("The current CWD is now: %s", cwd);
+
+    return 0;
 }
