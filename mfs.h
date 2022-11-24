40f48208bfcc40626658074a14bd8e5de78651a4
akizhao614 <jzhao19@sfsu.edu>
2022-11-23 21:03:01 -0800

    change the mfs.h and fix some function

--- mfs.h
+++ mfs.h
@@ -25,9 +25,10 @@
 
 #include "b_io.h"
 #include "fsLow.h"
+#include "helperFunctions.h"
 
 #include <dirent.h>
-#define FT_REGFILE DT_REG
+#define FT_REGFILE	DT_REG
 #define FT_DIRECTORY DT_DIR
 #define FT_LINK	DT_LNK
 
@@ -38,29 +39,44 @@ typedef u_int64_t uint64_t;
 typedef u_int32_t uint32_t;
 #endif
 
-#define MAX_DE 50
+// used with bitmap
 #define SPACE_IS_FREE 0
 #define SPACE_IN_USED 1
 
 #define DIR_TYPE 0
 #define FILE_TYPE 1
 
-#define MAX_PATHSIZE 255 //Max path size of any given file (255 bytes)
-
+#define MAX_ENTRIES_NUMBER 20
+#define MAX_NAME_LENGTH 256
 
 // This structure is returned by fs_readdir to provide the caller with information
 // about each file as it iterates through a directory
 struct fs_diriteminfo
 {
-	unsigned short d_reclen; /* length of this record */
-	unsigned char fileType;			// 0 -> dir, 1 -> file
-	char d_name[256];			  /* filename max filename is 255 characters */
-	uint64_t entry_StartLocation; // Variable for start location of either
-								  //  a file or directory
-	unsigned int isFreeOrUsed;	// to check current entry is in used or not
-	uint64_t fileSize;				// used to keep the file size in bytes
+	unsigned short d_reclen; 			/* length of this record */
+	unsigned char fileType;
+	unsigned char isFreeOrUsed;		  	// determine this entry is free or used
+	uint64_t directoryStartLocation;  	// LBA of the entry, either a file or directory
+	uint64_t fileSize;				  	// the exact size of the file occupies
+	char d_name[MAX_NAME_LENGTH]; 		/* filename max filename is 255 characters */
 };
 
+// used to hold a data file information about menu
+struct Directory_Entry
+{ 
+	unsigned short d_reclen; 			/* length of this record */
+	unsigned char fileType;				// 0->dir 1->file 
+	unsigned char dirUsed;		 		//to check if any Dir is in use, 0 is free, 1 is using 
+	uint64_t dir_Location;  			//integer variable to store file location 
+	size_t fileSize;				  	//variable for file size
+	char file_name[MAX_NAME_LENGTH]; 	//character variable to store file name 
+
+   	time_t create_time; 				//variable for file create time 
+ 	time_t last_access_time; 			//variable for when you access the file’s time
+	time_t modified_time; 				//variable for when you modify the file's time
+ 	// char comment [300]; 				// comment for the file 
+
+}Directory_Entry; 
 
 // This is a private structure used only by fs_opendir, fs_readdir, and fs_closedir
 // Think of this like a file descriptor but for a directory - one can only read
@@ -70,17 +86,51 @@ struct fs_diriteminfo
 typedef struct
 {
 	/*****TO DO:  Fill in this structure with what your open/read directory needs  *****/
-	unsigned short d_reclen;		 /*length of this record */
-	uint64_t directoryStartLocation; /*Starting LBA of directory */
-	unsigned short dirEntryCount;	 // amount of undeleted entries
-	char d_name[256];	 // name of this directory
-	struct fs_diriteminfo dirEntry[8];
-	// unsigned short dirEntryPosition; // we keep it as global value
+	unsigned short d_reclen;		 		/*length of this record */
+	uint64_t directoryStartLocation; 		/*Starting LBA of directory */
+	unsigned short dirEntryPosition;	 	/*which directory entry position, like file pos */
+	char d_name[MAX_NAME_LENGTH];	 		// name of this directory
+	struct Directory_Entry dirEntry[MAX_ENTRIES_NUMBER];
 } fdDir;
 
+//Struct for our VCB
+typedef struct VCB{
+	uint64_t magicNumber;				// integer variable for magic number, useful 
+										// when opening files based on their 
+										// file Signature, and also for hex dumps 
+	uint64_t blockSize;					// variable bytes for the size of each 
+										// blocks in VCB (512 bytes) 
+	uint64_t numberOfBlocks;	  		// integer variable for the number of blocks in VCB
+	uint VCB_blockCount;			  	//integer variable for starting position of free 
+										// block space VCB (2560 bytes)
+	uint freeSpace_BlockCount;	  		// used for check when it is not the first run
+	uint64_t current_FreeBlockIndex; 	// used for check when it is not the first run
+	uint64_t location_RootDirectory;	// can be calculated by adding the other two counts
+} volume_ControlBlock;
+
+//VCB related functions
+unsigned int getVCB_BlockCount(uint64_t bl_number);
+
+//DIR relate functions
+fdDir * parseEntry(struct Directory_Entry * entry);
+char * get_path_last_slash(char * path);
+fdDir * parsePath(char * name);
+
+int LBAwrtie_func(void * fdDir, uint64_t length, uint64_t startingPosition);
+
+//Global variables for VCB, FreeSpace, and Directory
+volume_ControlBlock * JCJC_VCB;
+fdDir * rootDir; // used for keep opened directory
+int * freespace;
+uint64_t openedDirEntryIndex;
+fdDir *fs_CWD;
+
+
+int releaseFreespace(uint64_t start, uint64_t count);
 
 // Key directory functions
 int fs_mkdir(const char *pathname, mode_t mode);
+int fs_mkFile(const char *pathname, mode_t mode);
 int fs_rmdir(const char *pathname);
 
 // Directory iteration functions
@@ -94,7 +144,6 @@ int fs_setcwd(char *pathname);   //linux chdir
 int fs_isFile(char * filename);	//return 1 if file, 0 otherwise
 int fs_isDir(char * pathname);		//return 1 if directory, 0 otherwise
 int fs_delete(char* filename);	//removes a file
-
 
 // This is the strucutre that is filled in from a call to fs_stat
 struct fs_stat
@@ -111,76 +160,4 @@ struct fs_stat
 
 int fs_stat(const char *path, struct fs_stat *buf);
 
-//Struct for our VCB
-typedef struct VCB{
-
-	uint64_t numberOfBlocks;  			//integer variable for the number of blocks in VCB
-	uint64_t blockSize;					//variable bytes for the size of each blocks in VCB (512 bytes) 
-
-	unsigned int VCB_blockCount; 		//integer variable for starting position of free block space VCB (2560 bytes)
-	uint64_t freeSpace_BlockCount; 		//integer variable for block count of free space in VCB
-	uint64_t current_FreeBlockIndex;	//integer index variable for current free space starting position in VCB
-	uint64_t first_freespace;
-	
-	uint64_t magicNumber;				//integer variable for magic number, useful when opening files based on their 
-										//file Signature, and also for hex dumps 
-					
-
-	// int * free_block_location;			//the pointer to track our free space
-	unsigned int free_block_count;		//the total numbers of the free blocks
-	uint64_t location_RootDirectory;	//integer variable to hold root directory location in VCB
-
-}volume_ControlBlock;
-
-
-//VCB related functions
-unsigned int getVCB_BlockCount(uint64_t)
-unsigned int getVCB_num_bytes(uint64_t)
-fdDir * parse_DirectoryPath(char *);
-
-//DIR relate functions
-fdDir * get_dir_entry(struct fs_diriteminfo *entry);
-char * get_path_last_slash(char * path);
-fdDir * get_dir_path(char * name);
-
-
-//Global variables for VCB, FreeSpace, and Directory
-volume_ControlBlock * JCJC_VCB;
-fdDir * rootDir_ptr;
-int * freespace;
-fdDir * directories; // openDir
-uint64_t openedDirEntryIndex;
-fdDir * fs_CWD;
-int length_of_dir;
-int block_we_have;
-
-
-int init_VCB (uint64_t numberOfBlocks, uint64_t blockSize, __u_int blockCount_VCB);
-void exitFileSystem ();
-int init_freeSpace (volume_ControlBlock * JCJC_VCB);
-int init__RootDir ();
-
-
-
-
-typedef struct Directory_Entry{
-
-	char file_name[256]; 				//character variable to store file name
-	// unsigned int dir_Location;			//integer variable to store file location
-	size_t fileSize;					//variable for file size
-	int dirUsed;						//to check if any Dir is in use, 0 is free, 1 is using
-	
-	int fileType;						// 0->dir    1->file
-	char filePath[256];				 	//file path
-	// unsigned directory_entry; 		// there is 60 bytes directory entry
-
-	// time_t create_date; 				//variable for file create date
-	// time_t last_access_date; 		//variable for when you access/modify the file’s date
-	// char  comment [300]; 			// comment for the file
-
-}Directory_Entry;
-
-
-
 #endif
-
