302ef77efbd4dc1cbb9ea9be03cdfdbb71ac2dc9
akizhao614 <jzhao19@sfsu.edu>
2022-11-23 21:05:51 -0800

    update helperFunction header

--- helperFunctions.h
+++ helperFunctions.h
@@ -17,16 +17,9 @@
 
 
 #include "mfs.h"
-#include "bitmap.c"
 
-unsigned int getVCB_BlockCount(uint64_t bl_number);
-
-int allocateFreeSpace_Bitmap(int block_count_needed);
-
-unsigned int getVCB_num_bytes(uint64_t block_count);
-
-int checkBit(uint64_t block_index, int * freespace);
-
-void setBitUsed(uint64_t block_index, int * freespace);
-
-void setBitFree(uint64_t block_index, int * freespace);
+uint64_t allocateFreeSpace_Bitmap(uint64_t block_ToBeAllocated);
+int setUsed(uint64_t, int *bitmap);
+int setBitFree(uint64_t, int *bitmap);
+int checkBit(uint64_t, int *bitmap);
+int convertBitToBytes();
